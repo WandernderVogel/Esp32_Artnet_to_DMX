bf603a52a38c475aefc05b82fac20ee195de48c6
WandernderVogel <105592395+WandernderVogel@users.noreply.github.com>
2025-09-15 21:47:43 +0200

    Update main.cpp for use of two dmx ports

--- ESP32_Artnet_to_DMX/src/main.cpp
+++ ESP32_Artnet_to_DMX/src/main.cpp
@@ -1,14 +1,31 @@
+/**
+ * @ Name: Artent to DMX
+ * @ Description: Artnet to DMX converter
+ * @ Author: Wandernder Vogel
+ * @ Create Time: 2025-09-15 12:08
+ * @ Modified by: Wandernder Vogel
+ * @ Modified time: 2025-09-15 20:53
+ */
+
 #include <Arduino.h>
 #include <ArtnetnodeWifi.h>
 #include <easydmx.h>
+#include <SoftwareSerial.h>
 
 // works for now, needs to be more efficent because lag is just too much
 
 ArtnetnodeWifi artnet;
 WiFiUDP udp;
 
+// ========================================================
+// user defined vars PLEASE CHANGE BEFORE RUNNING
+
 const char *ssid = "Your_WiFi_SSID";
 const char *pwd = "Your_WiFi_PWD";
+
+const int max_universes = 2;
+
+// ========================================================
 
 const int startingUniverse = 0;
 const int chan_count = 512;
@@ -22,9 +39,14 @@ IPAddress subnet(255, 255, 255, 0); // subnet
 
 const int tx_pin = 16;
 const int rx_pin = 17; // not used, can be used if code is altered and device should be used for DMX-> Artnet
+const int tx2_pin = 19;
+const int rx2_pin = 21; // not used, can be used if code is altered and device should be used for DMX-> Artnet
 
-uint8_t dmx_data[512] = {0};
+uint8_t dmx_data[max_universes][512] = {{0}, {0}};
 EasyDMX dmx;
+
+SoftwareSerial Serial3(rx2_pin, tx2_pin);
+EasyDMX dmx2;
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data);
 void initWiFi();
@@ -43,6 +65,7 @@ void setup()
   artnet.begin();
 
   dmx.begin(DMXMode::Transmit, DMXPin::NoRx, DMXPin::Serial2Tx);
+  dmx2.begin(DMXMode::Transmit, DMXPin::NoRx, tx2_pin);
 }
 
 void loop()
@@ -52,40 +75,36 @@ void loop()
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data)
 {
-  if (universe == startingUniverse)
+  // Serial.print("DMX: Univ: ");
+  // Serial.print(universe, DEC);
+  // Serial.print(", Seq: ");
+  // Serial.print(sequence, DEC);
+  // Serial.print(", Data (");
+  // Serial.print(length, DEC);
+  // Serial.print("): ");
+
+  if (length > chan_count)
   {
-
-    // Serial.print("DMX: Univ: ");
-    // Serial.print(universe, DEC);
-    // Serial.print(", Seq: ");
-    // Serial.print(sequence, DEC);
-    // Serial.print(", Data (");
-    // Serial.print(length, DEC);
-    // Serial.print("): ");
-
-    if (length > chan_count)
-    {
-      length = chan_count;
-    }
-
-    // send out the buffer
-    // give every universe its own place in one long array
-    for (int i = 0; i < length; i++)
-    {
-      // do stuff for all chanels
-
-      // print hex data to Serial console(makes the program slower)
-      // Serial.print(data[i], HEX);
-      // Serial.print(" ");
-      if (dmx_data[i] != data[i])
-      {
-        dmx.setChannel(i + 1, data[i]);
-      }
-      dmx_data[i] = data[i];
-    }
-    // Serial.println()
-    // do fancy stuff for single chanels
+    length = chan_count;
   }
+
+  // send out the buffer
+  // give every universe its own place in one long array
+  for (int i = 0; i < length; i++)
+  {
+    // do stuff for all chanels
+
+    // print hex data to Serial console(makes the program slower)
+    // Serial.print(data[i], HEX);
+    // Serial.print(" ");
+    if (dmx_data[universe][i] != data[i])
+    {
+      (universe == startingUniverse) ? dmx.setChannel(i + 1, data[i]) : dmx2.setChannel(i + 1, data[i]);
+    }
+    dmx_data[universe][i] = data[i];
+  }
+  // Serial.println()
+  // do fancy stuff for single chanels
 }
 
 void initWiFi()
